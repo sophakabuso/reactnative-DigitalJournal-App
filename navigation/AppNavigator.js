f8b6c15d2edeb94f63f5e152d33fb627d029af1d
sophakabuso <sopha.buso@gmail.com>
2023-08-17 15:46:09 +0200

    update

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import VoiceNoteListScreen from '../screens/VoiceNoteListScreen'; // Update import path
-import RecordingScreen from '../screens/RecordingScreen'; // Update import path
-import PlayScreen from '../screens/PlayScreen'; // Update import path
+import VoiceNoteListScreen from '../screens/VoiceNoteListScreen';
+import RecordingScreen from '../screens/RecordingScreen';
+import PlayScreen from '../screens/PlayScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="VoiceNoteList">
-      <Stack.Screen name="VoiceNoteList" component={VoiceNoteListScreen} />
-      <Stack.Screen name="Record" component={RecordingScreen} />
-      <Stack.Screen name="Play" component={PlayScreen} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="VoiceNoteList">
+        <Stack.Screen name="VoiceNoteList" component={VoiceNoteListScreen} />
+        <Stack.Screen name="Record" component={RecordingScreen} />
+        <Stack.Screen name="Play" component={PlayScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
